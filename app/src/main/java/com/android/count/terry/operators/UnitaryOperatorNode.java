7cc4271a557b58d6326623ea4ed7db85ea052251
Terry Wang <terry_wang@hiiir.com>
2015-12-13 03:36:07 +0800

    @fix the spellings... XDDD

--- app/src/main/java/com/android/count/terry/operators/UnitaryOperatorNode.java
+++ app/src/main/java/com/android/count/terry/operators/UnitaryOperatorNode.java
@@ -5,10 +5,10 @@ import com.android.count.terry.INode;
 /**
  * Created by Terry Wang on 2015/12/13.
  */
-public abstract class UniaryOperatorNode implements INode {
+public abstract class UnitaryOperatorNode implements INode {
     protected final INode mNode;
 
-    public UniaryOperatorNode(INode node){
+    public UnitaryOperatorNode(INode node){
         mNode = node;
     }
 }
--- app/src/main/java/com/android/count/terry/operators/uniaryOperators/AbsoluteValueNode.java
+++ app/src/main/java/com/android/count/terry/operators/uniaryOperators/AbsoluteValueNode.java
@@ -1,12 +1,12 @@
 package com.android.count.terry.operators.uniaryOperators;
 
 import com.android.count.terry.INode;
-import com.android.count.terry.operators.UniaryOperatorNode;
+import com.android.count.terry.operators.UnitaryOperatorNode;
 
 /**
  * Created by Terry Wang on 2015/12/13.
  */
-public class AbsoluteValueNode extends UniaryOperatorNode {
+public class AbsoluteValueNode extends UnitaryOperatorNode {
 
     public AbsoluteValueNode(INode node) {
         super(node);
